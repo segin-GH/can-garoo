078367ee083e52c5f9b87af6080004a6d823d196
segin-GH <segin.bytes@gmail.com>
2023-10-29 21:50:41 +0530

    feat: added websockets

--- cangaroo/components/web_server/web_server.c
+++ cangaroo/components/web_server/web_server.c
@@ -1,6 +1,7 @@
 /* TODO Make webpage loading fast*/
 /* TODO mv the init of spiffs to init server and copy the file into buffer
         before the default uri request comes */
+/* TODO when a unknown route is called the the core panics */
 
 #include <stdio.h>
 #include "web_server.h"
@@ -9,6 +10,66 @@
 static const char *HTTP_TAG = "HTTP_SERVER";
 static httpd_handle_t server = NULL;
 
+#define WS_MAX_SIZE 1024
+int client_session_id;
+
+static esp_err_t send_text_response(httpd_req_t *req, const char *response_text);
+static esp_err_t handle_web_socket_frame(httpd_req_t *req);
+
+static esp_err_t on_web_socket_url(httpd_req_t *req)
+{
+    client_session_id = httpd_req_to_sockfd(req);
+    if (req->method == HTTP_GET)
+    {
+        return ESP_OK;
+    }
+
+    esp_err_t err = handle_web_socket_frame(req);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(HTTP_TAG, "Failed to handle websocket frame (%s)", esp_err_to_name(err));
+    }
+
+    return err;
+}
+
+static esp_err_t handle_web_socket_frame(httpd_req_t *req)
+{
+    httpd_ws_frame_t ws_pkt;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+    ws_pkt.payload = malloc(WS_MAX_SIZE);
+    if (ws_pkt.payload == NULL)
+    {
+        return ESP_ERR_NO_MEM;
+    }
+
+    esp_err_t err = httpd_ws_recv_frame(req, &ws_pkt, WS_MAX_SIZE);
+    if (err != ESP_OK)
+    {
+        free(ws_pkt.payload);
+        return err;
+    }
+
+    printf("payload: %.*s\n", ws_pkt.len, ws_pkt.payload);
+    free(ws_pkt.payload);
+
+    return send_text_response(req, "connected ok :) ");
+}
+
+static esp_err_t send_text_response(httpd_req_t *req, const char *response_text)
+{
+    httpd_ws_frame_t ws_response = {
+        .final = true,
+        .fragmented = false,
+        .type = HTTPD_WS_TYPE_TEXT,
+        .payload = (uint8_t *)response_text,
+        .len = strlen(response_text),
+    };
+
+    return httpd_ws_send_frame(req, &ws_response);
+}
 static esp_err_t default_url(httpd_req_t *req)
 {
     esp_err_t err;
@@ -83,6 +144,18 @@ esp_err_t init_http_server(void)
     if (err != ESP_OK)
     {
         ESP_LOGE(HTTP_TAG, "Error starting server: %s", esp_err_to_name(err));
+        return err;
+    }
+    httpd_uri_t web_socket_url = {
+        .uri = "/ws",
+        .method = HTTP_GET,
+        .handler = on_web_socket_url,
+        .is_websocket = true};
+    err = httpd_register_uri_handler(server, &web_socket_url);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(HTTP_TAG, "Error registering URI handler: %s", esp_err_to_name(err));
+        deinit_http_server();
         return err;
     }
 
